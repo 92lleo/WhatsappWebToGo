77911ea945ffea93596474f5b5b043256320efb1
Leonhard Künzler <leonhard@kuenzler.io>
2018-02-09 22:50:30 +0100

    Add/fix keyboard auto-block #2

--- app/src/main/java/io/kuenzler/whatsappwebtogo/MainActivity.java
+++ app/src/main/java/io/kuenzler/whatsappwebtogo/MainActivity.java
@@ -44,6 +44,9 @@ import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
+    private static final String listenerJS = "javascript:var listenerAlreadyAdded; if(listenerAlreadyAdded==null){listenerAlreadyAdded = false;} var clickEventFunc = function(e){if(e.target && (e.target.className== 'pluggable-input-body copyable-text selectable-text' || e.target.id == 'input-chatlist-search')){TFCLI.hitSomething(e.target.className+''); e.target.click(); } else {TFCLI.hitNothing(e.target.className+'');}}; if(!listenerAlreadyAdded){document.addEventListener('click',clickEventFunc); listenerAlreadyAdded = true;}";
+    //private static final String listenerJS = "javascript:document.addEventListener('click',function(e){  if(e.target && (e.target.className== 'pluggable-input-body copyable-text selectable-text' " +
+    //        "|| e.target.id == 'input-chatlist-search')){TFCLI.hitSomething(); } else {TFCLI.hitNothing(e.target.className+'')} })";
 
     // private static final String js = "function wrapFunc(name) {alert('works!); if (typeof window[name] == 'function') {var original = window['__' + name] = window[name]; window[name] = function() { var result = original.apply(this, arguments); Interceptor.reportCall(name, result.toString()); return result; } alert('yes'); } else { alert('no'); }}";
 
