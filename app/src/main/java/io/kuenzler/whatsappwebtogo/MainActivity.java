9761c87360dc05384487dcbf5c901b8705cece7a
Leonhard Künzler <leonhard@kuenzler.io>
2020-01-20 21:09:21 +0100

    Remove Crosswalk impl and dependencies

--- app/src/main/java/io/kuenzler/whatsappwebtogo/MainActivity.java
+++ app/src/main/java/io/kuenzler/whatsappwebtogo/MainActivity.java
@@ -1,21 +1,18 @@
 package io.kuenzler.whatsappwebtogo;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity  {
 
     private static final String DEBUG_TAG = "WAWEBTOGO";
 
-    private final Activity activity = this;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Intent myIntent = new Intent(MainActivity.this, WebviewActivity.class);
-        //Intent myIntent = new Intent(MainActivity.this, CrosswalkActivity.class);
         MainActivity.this.startActivity(myIntent);
         finish();
     }
