2723340207ca755bec27f99203faa4bc7eff25e4
firebrain12 <84021489+firebrain12@users.noreply.github.com>
2021-05-19 11:01:30 -0400

    Add files via upload

---
+++ app/src/main/java/io/kuenzler/whatsappwebtogo/BlobConverter.java
@@ -0,0 +1,144 @@
+package io.kuenzler.whatsappwebtogo;
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Environment;
+import android.os.Handler;
+import android.util.Base64;
+import android.webkit.JavascriptInterface;
+import android.webkit.MimeTypeMap;
+import android.widget.Toast;
+
+import androidx.core.app.NotificationCompat;
+import androidx.core.content.FileProvider;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.util.Date;
+
+public class BlobConverter {
+    private Context context;
+    public BlobConverter(Context context) {
+        this.context = context;
+    }
+
+
+    @JavascriptInterface
+    public void getBase64FromBlobData(String base64Data) throws IOException {
+        convertBase64StringToPdfAndStoreIt(base64Data);
+    }
+    public static String getBase64StringFromBlobUrl(String blobUrl) {
+        if(blobUrl.startsWith("blob")){
+            return "javascript: var xhr = new XMLHttpRequest();" +
+                    "xhr.open('GET', '"+ blobUrl +"', true);" +
+                    "xhr.responseType = 'blob';" +
+                    "xhr.onload = function(e) {" +
+                    "    if (this.status == 200) {" +
+                    "        var blobPdf = this.response;" +
+                    "        var reader = new FileReader();" +
+                    "        reader.readAsDataURL(blobPdf);" +
+                    "        reader.onloadend = function() {" +
+                    "            base64data = reader.result;" +
+                    "            Android.getBase64FromBlobData(base64data);" +
+                    "        }" +
+                    "    }" +
+                    "};" +
+                    "xhr.send();";
+
+        }
+        return "javascript: console.log('It is not a Blob URL');";
+    }
+    private void convertBase64StringToPdfAndStoreIt(String base64PDf) throws IOException {
+        final int notificationId = 1;
+        String currentDateTime = DateFormat.getDateTimeInstance().format(new Date());
+        String[] strings = base64PDf.split(",");
+        String extension;
+        switch (strings[0]) {//check image's extension
+            case "data:image/jpeg;base64":
+                extension = ".jpeg";
+                break;
+            case "data:image/png;base64":
+                extension = ".png";
+                break;
+            case "data:video/mp4;base64":
+                extension = ".mp4";
+                break;
+            case "data:video/3gp;base64":
+                extension = ".3gp";
+                break;
+            case "data:video/avi;base64":
+                extension = ".avi";
+                break;
+            case "data:image/gif;base64":
+                extension = ".gif";
+                break;
+            case "data:application/pdf;base64":
+                extension = ".pdf";
+                break;
+            default://should write cases for more images types
+                extension = "jpg";
+                break;
+        }
+
+        final File dwldsPath = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS)+" "+currentDateTime+"downloaded" + extension);
+        byte[] pdfAsBytes = Base64.decode(base64PDf.replaceFirst(strings[0], ""), 0);
+        FileOutputStream os;
+        os = new FileOutputStream(dwldsPath, false);
+        os.write(pdfAsBytes);
+        os.flush();
+
+        if (dwldsPath.exists()) {
+            Intent intent = new Intent();
+            intent.setAction(Intent.ACTION_VIEW);
+            Uri apkURI = FileProvider.getUriForFile(context,context.getApplicationContext().getPackageName() + ".provider", dwldsPath);
+            intent.setDataAndType(apkURI, MimeTypeMap.getSingleton().getMimeTypeFromExtension(extension));
+            intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+            PendingIntent pendingIntent = PendingIntent.getActivity(context,1, intent, PendingIntent.FLAG_CANCEL_CURRENT);
+            String CHANNEL_ID = "MYCHANNEL";
+            final NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+
+            if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+                NotificationChannel notificationChannel= new NotificationChannel(CHANNEL_ID,"name", NotificationManager.IMPORTANCE_LOW);
+                Notification notification = new Notification.Builder(context,CHANNEL_ID)
+                        .setContentText("You have got something new!")
+                        .setContentTitle("File downloaded")
+                        .setContentIntent(pendingIntent)
+                        .setChannelId(CHANNEL_ID)
+                        .setSmallIcon(android.R.drawable.sym_action_chat)
+                        .build();
+                if (notificationManager != null) {
+                    notificationManager.createNotificationChannel(notificationChannel);
+                    notificationManager.notify(notificationId, notification);
+                }
+
+            } else {
+                NotificationCompat.Builder b = new NotificationCompat.Builder(context, CHANNEL_ID)
+                        .setDefaults(NotificationCompat.DEFAULT_ALL)
+                        .setWhen(System.currentTimeMillis())
+                        .setSmallIcon(android.R.drawable.sym_action_chat)
+                        //.setContentIntent(pendingIntent)
+                        .setContentTitle("Download")
+                        .setContentText("Downloaded");
+
+                if (notificationManager != null) {
+                    notificationManager.notify(notificationId, b.build());
+                    Handler h = new Handler();
+                    long delayInMilliseconds = 1000;
+                    h.postDelayed(new Runnable() {
+                        public void run() {
+                            notificationManager.cancel(notificationId);
+                        }
+                    }, delayInMilliseconds);
+                }
+            }
+        }
+        Toast.makeText(context, "Saved to Downloads folder!", Toast.LENGTH_SHORT).show();
+    }
+}
